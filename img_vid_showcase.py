cd345e8e0f9492079767b05a832c8dd0c5549fa2
PaltaConQueso <47439085+lukitasxue@users.noreply.github.com>
2025-02-07 22:51:18 +1100

    Update img_vid_showcase.py

--- img_vid_showcase.py
+++ img_vid_showcase.py
@@ -8,16 +8,23 @@ print("OpenCV Version:", cv.__version__)
 # cv.waitKey(0)  # Press any key to close the window
 
 #reading videos 
-capture = cv.VideoCapture('videos/test_chamber.mp4') #numer reference the camera number
+# capture = cv.VideoCapture(0) #numer reference the camera number
 
-while True:
-    isTrue, frame = capture.read()
-    cv.imshow('Video', frame)
+# while True:
+#     isTrue, frame = capture.read() #capture frame by frame
+#     cv.imshow('Video', frame)
 
-    if cv.waitKey(20) & 0xFF==ord('d'): #if press d, destroy the window
-        break
+#     if cv.waitKey(20) & 0xFF==ord('q'): #if press q, destroy the window
+#         break
+# capture.release()
 
-capture.release()
+# convert to gray an image
+img = cv.imread("images/test.png")  # Load an image
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # Convert to grayscale
+
+cv.imshow("Grayscale Image", gray)
+cv.waitKey(0)
+
+
 
 cv.destroyAllWindows()
-
